8097327289334e18ee9140681e6a3dc8e85abde9
Glitch (ripe-impulse) <none>
2018-01-21 23:43:39 +0000

    🎺🏖 Checkpoint views/index.html:603188/457 public/style.css:603188/114 server.js:603188/3

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.get("/", function (request, response) {
 
 
 
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
