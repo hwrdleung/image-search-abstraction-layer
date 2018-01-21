65a68b5f616597471af9b58120b77d5a1b2b6c97
Glitch (ripe-impulse) <none>
2018-01-21 23:54:39 +0000

    💒🏵 Checkpoint server.js:603188/565

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// server.js
-// where your node app starts
+
 
 // init project
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -15,6 +15,22 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("latest/imagesearch", function(request, response) {
+  //get recent searches from database
+  //display recent searches in JSON format
+});
+
+app.get("/:userInput", function(request, response){
+  //get userInput
+  //get offset from userInput
+  //save search query to search history in database
+  //perform search
+  //display search results in JSON format
+});
+        
+
+        
 
 
 
