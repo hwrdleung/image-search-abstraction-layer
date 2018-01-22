1dd4c850373f4e54ad1b300d40ac109d40c1a1e7
Glitch (ripe-impulse) <none>
2018-01-22 00:17:19 +0000

    😇🍪 Checkpoint server.js:603188/471

--- server.js
+++ server.js
@@ -4,29 +4,34 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+var bing = require('node-bing-api')({accKey: 'PASTEKEYHERE'});
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyparser.json());
+app.use(cors());
 
-// http://expressjs.com/en/starter/basic-routing.html
+//get calls
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("latest/imagesearch", function(request, response) {
+app.get("/api/latest/imagesearch", function(request, response) {
   //get recent searches from database
   //display recent searches in JSON format
 });
 
-app.get("/:userInput", function(request, response){
+app.get("/api/imagesearch/:searchVal*", function(request, response){
   //get userInput
   //get offset from userInput
   //save search query to search history in database
   //perform search
   //display search results in JSON format
+  
+  var { searchVal } = request.params;
+  var { offset }
+  
 });
         
 
